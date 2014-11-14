4b264f7fca3b2eb95179f4cca42f6e5bbf07a75b
santoshfaculty1 <nouser@noemail.com>
2014-11-14 18:28:21 +0530

    Initial Draft

---
+++ src/Test9.java
@@ -0,0 +1,16 @@
+/**
+ * Main class
+ */
+public class Test9 {
+    /**
+    * Main method
+    * @param args 
+    */
+    public static void main(String[] args) {    	
+		System.out.println("Hello world");
+    }
+    //Student code should go here.
+    public void push(Object o){
+    	//Write a code to push an object on stack...
+    }
+}
